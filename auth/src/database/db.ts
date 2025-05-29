f7e4fbcc92c0aada097737cd0e7ec675a6e4d332
Batuhan Kas <batuhannkas@gmail.com>
2025-05-29 23:49:50 +0300

    build fixes

--- auth/src/database/db.ts
+++ auth/src/database/db.ts
@@ -1,7 +1,13 @@
 import Database from 'better-sqlite3';
 import path from 'path';
+import fs from 'fs';
 
-const DB_PATH = path.resolve(__dirname, './sqlite/data.sqlite');
+const DB_DIRECTORY = path.resolve(__dirname, './sqlite');
+const DB_PATH = path.resolve(DB_DIRECTORY, 'data.sqlite');
+
+if (!fs.existsSync(DB_DIRECTORY)) {
+    fs.mkdirSync(DB_DIRECTORY, { recursive: true });
+}
 
 const db = new Database(DB_PATH);
 
