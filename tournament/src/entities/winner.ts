647a190ce9cc0d12ee55e5d993f0048e05dbf8f4
Batuhan Kas <batuhannkas@gmail.com>
2025-07-10 23:15:31 +0300

    refactor: MatchParticipant added.

--- tournament/src/entities/winner.ts
+++ tournament/src/entities/winner.ts
@@ -4,3 +4,11 @@ export type Winner = {
     round_number: number;
     winner: Participant;
 };
+
+/**
+ * Represents a participant in a match, including the round number and participant details.
+ */
+export type MatchParticipant = {
+    round_number: number;
+    participant: Participant;
+}
