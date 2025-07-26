c9a4692808916bbcdc9b2bc49b7017524cb2ca45
Batuhan Kas <batuhannkas@gmail.com>
2025-07-26 15:43:10 +0300

    fix: middleware.

--- tournament/src/middleware/middleware.ts
+++ tournament/src/middleware/middleware.ts
@@ -8,7 +8,7 @@ import Result from "../bean/result";
  * @param request
  */
 export async function authMiddleware(request: FastifyRequest) {
-	if ((request.headers.x_api_key) === process.env.X_API_KEY) {
+	if ((request.headers['x-api-key']) === process.env.X_API_KEY) {
 		return new Result(StatusCodes.OK, {}, "Token is valid");
 	}
 
