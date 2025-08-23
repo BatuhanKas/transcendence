e9250cf1b0eaa36c37b51c7dbeeedac917f63448
root <root@debian.debian>
2025-08-23 17:51:32 -0400

    root

--- auth/src/services/mail.service.ts
+++ auth/src/services/mail.service.ts
@@ -11,7 +11,7 @@ const emailCache = new Map<string, string>();
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.PORT),
+    port: Number(process.env.SMTP_PORT),
     secure: process.env.SMTP_SECURE === 'false',
     auth: {
         user: process.env.SMTP_USER,
